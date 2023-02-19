489faee2ff00e42b0e4c13a66c8ec694c5b544f8
kwonjoosung <kwonjoosung@gmail.com>
2023-02-20 04:19:07 +0900

    feat(base): BaseEntity 추가
    - 생성 일자
    - 수정 일자
    - 삭제 여부

---
+++ src/main/java/com/prgrms/mukvengers/global/common/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.prgrms.mukvengers.global.common.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+	@CreatedDate
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	private boolean deleted;
+}
