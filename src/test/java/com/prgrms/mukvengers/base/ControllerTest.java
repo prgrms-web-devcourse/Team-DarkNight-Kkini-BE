fac4340747abdbe0cfebf9945ae0a3396e13be60
kwonjoosung <kwonjoosung@gmail.com>
2023-02-20 05:22:24 +0900

    config(test): RestDocs를 Swagger로 변환하기 위한 설정
    - setUpRestDocs 메소드를 통해 기본 설정 셋업

--- src/test/java/com/prgrms/mukvengers/base/ControllerTest.java
+++ src/test/java/com/prgrms/mukvengers/base/ControllerTest.java
@@ -1,11 +1,48 @@
 package com.prgrms.mukvengers.base;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.prgrms.mukvengers.config.RestDocsConfig;
 
 @Transactional
+@ExtendWith(RestDocumentationExtension.class)
+@Import({RestDocsConfig.class})
 @SpringBootTest
 public abstract class ControllerTest {
 
+	@Autowired
+	protected RestDocumentationResultHandler restDocs;
+
+	@Autowired
+	protected ObjectMapper objectMapper;
+
+	protected MockMvc mockMvc;
+
+	@BeforeEach
+	void setUpRestDocs(WebApplicationContext webApplicationContext,
+		RestDocumentationContextProvider restDocumentation) {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+			.addFilter(new CharacterEncodingFilter("UTF-8", true))
+			.apply(documentationConfiguration(restDocumentation))
+			.apply(springSecurity())
+			.alwaysDo(restDocs)
+			.build();
+	}
 
 }
