468e9de8b47766831a30b236c0de8c735d0a0edc
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-02-20 23:53:10 +0900

    feat(Store): Store Entity 추가

---
+++ src/main/java/com/prgrms/mukvengers/domain/store/model/Store.java
@@ -0,0 +1,39 @@
+package com.prgrms.mukvengers.domain.store.model;
+
+import static javax.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.locationtech.jts.geom.Point;
+
+import com.prgrms.mukvengers.global.common.domain.BaseEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Where(clause = "deleted = false")
+@SQLDelete(sql = "UPDATE store set deleted = true where id=?")
+public class Store extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = AUTO)
+	private Long id;
+
+	@Column(nullable = false)
+	private Point location;
+
+	@Builder
+	protected Store(Point location) {
+		this.location = location;
+	}
+}
