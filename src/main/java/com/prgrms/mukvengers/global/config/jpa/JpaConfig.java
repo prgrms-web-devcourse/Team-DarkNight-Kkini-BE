17800c7d8dc5ea15024d6c2832aaee4edec938a9
kwonjoosung <kwonjoosung@gmail.com>
2023-02-22 02:25:21 +0900

    test(User, Repo): UserRepositoryTest 구현
    - create_at 필드가 NOT NULL이기 때문에 JPAConfig(EnableJpaAuditing)가 필요
    - 따라서, RepositoryTest에 @Import(JpaConfig.class)를 추가
    
    Signed-off-by: kwonjoosung <kwonjoosung@gmail.com>

---
+++ src/main/java/com/prgrms/mukvengers/global/config/jpa/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.prgrms.mukvengers.global.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
+
--- src/test/java/com/prgrms/mukvengers/base/RepositoryTest.java
+++ src/test/java/com/prgrms/mukvengers/base/RepositoryTest.java
@@ -1,7 +1,11 @@
 package com.prgrms.mukvengers.base;
 
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
+import com.prgrms.mukvengers.global.config.jpa.JpaConfig;
+
+@Import(JpaConfig.class)
 @DataJpaTest
 public abstract class RepositoryTest {
 
