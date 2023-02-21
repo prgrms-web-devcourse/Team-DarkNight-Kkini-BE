0acf725036ddc8da133a5dbd6800ac29a8b7e7e6
kwonjoosung <kwonjoosung@gmail.com>
2023-02-22 02:59:36 +0900

    test(User, Service): UserServiceTest 추가
    - RUD 테스트
    
    Signed-off-by: kwonjoosung <kwonjoosung@gmail.com>

---
+++ src/test/java/com/prgrms/mukvengers/domain/user/service/UserServiceTest.java
@@ -0,0 +1,100 @@
+package com.prgrms.mukvengers.domain.user.service;
+
+import static com.prgrms.mukvengers.utils.UserObjectProvider.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.prgrms.mukvengers.base.ServiceTest;
+import com.prgrms.mukvengers.domain.user.dto.request.UpdateUserRequest;
+import com.prgrms.mukvengers.domain.user.dto.response.UserProfileResponse;
+import com.prgrms.mukvengers.domain.user.exception.UserNotFoundException;
+import com.prgrms.mukvengers.domain.user.mapper.UserMapper;
+import com.prgrms.mukvengers.domain.user.model.User;
+import com.prgrms.mukvengers.domain.user.repository.UserRepository;
+
+class UserServiceTest extends ServiceTest {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserMapper userMapper;
+
+	@Nested
+	@DisplayName("#3 유저 CRUD 기능")
+	class CRUDTest {
+
+		User savedUser;
+
+		@BeforeEach
+		void setUp() {
+			savedUser = userRepository.save(createUser());
+		}
+
+		@Test
+		@DisplayName("[성공] userId를 통해서 사용자 프로필를 조회할 수 있다.")
+		void getUserProfileSuccessTest() {
+			// given & when
+			UserProfileResponse userProfile = userService.getUserProfile(savedUser.getId());
+			//then
+			assertThat(userProfile)
+				.isEqualTo(userMapper.toSingleUserResponse(savedUser));
+		}
+
+		@DisplayName("[성공] 사용자의 프로필(닉네임, 프로필 이미지, 자기소개)를 한번에 수정할 수 있다.")
+		@Test
+		void updateUserProfileSuccessTest() {
+			//given
+			UpdateUserRequest request = new UpdateUserRequest("테스트2", "https://updateImg.jpg", "수정된 자기소개");
+			// when
+			UserProfileResponse userProfile = userService.updateUserProfile(request, savedUser.getId());
+			// then
+			assertThat(userProfile)
+				.hasFieldOrPropertyWithValue("id", savedUser.getId())
+				.hasFieldOrPropertyWithValue("nickname", "테스트2")
+				.hasFieldOrPropertyWithValue("profileImgUrl", "https://updateImg.jpg")
+				.hasFieldOrPropertyWithValue("introduction", "수정된 자기소개")
+				.hasFieldOrPropertyWithValue("leaderCount", 0)
+				.hasFieldOrPropertyWithValue("crewCount", 0)
+				.hasFieldOrPropertyWithValue("tasteScore", 0)
+				.hasFieldOrPropertyWithValue("mannerScore", 36.5)
+			;
+		}
+
+		@DisplayName("[성공] 사용자 정보를 삭제할 수 있다.")
+		@Test
+		void deleteUserSuccessTest() {
+			assertDoesNotThrow(
+				() -> userService.deleteUser(savedUser.getId())
+			);
+			// 삭제 후 조회
+			assertThatThrownBy(() -> userService.getUserProfile(savedUser.getId()))
+				.isInstanceOf(UserNotFoundException.class);
+		}
+
+		@DisplayName("[실패] userId에 해당하는 사용자가 없으면 UserNotFoundException")
+		@Test
+		void notFoundMatchUser() {
+			Long UNSAVED_USER_ID = 0L;
+			assertAll(
+				() -> assertThatThrownBy(() -> userService.getUserProfile(UNSAVED_USER_ID))
+					.isInstanceOf(UserNotFoundException.class),
+				() -> assertThatThrownBy(() -> userService.updateUserProfile(getUpdateUserRequest(), UNSAVED_USER_ID))
+					.isInstanceOf(UserNotFoundException.class),
+				() -> assertThatThrownBy(() -> userService.deleteUser(UNSAVED_USER_ID))
+					.isInstanceOf(UserNotFoundException.class)
+			);
+		}
+	}
+
+}
