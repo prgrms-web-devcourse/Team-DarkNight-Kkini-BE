4eb7ca35374baaef17cb9dd159d2625760f0aff7
kwonjoosung <kwonjoosung@gmail.com>
2023-02-22 02:33:41 +0900

    test(Util): UserObjectProvider
    - 테스트 코드에서 편의를 위해 유틸성 ObjectProvider 클래스 추가
    
    Signed-off-by: kwonjoosung <kwonjoosung@gmail.com>

---
+++ src/test/java/com/prgrms/mukvengers/utils/UserObjectProvider.java
@@ -0,0 +1,30 @@
+package com.prgrms.mukvengers.utils;
+
+import com.prgrms.mukvengers.domain.user.dto.request.UpdateUserRequest;
+import com.prgrms.mukvengers.domain.user.model.User;
+
+public class UserObjectProvider {
+
+	public static final String DEFAULT_NICKNAME = "끼니";
+	public static final String DEFAULT_PROFILE_IMG_URL = "https://defaultImg.jpg";
+	public static final String PROVIDER_KAKAO = "kakao";
+	public static final String OAUTH_ID = "12345";
+
+	public static User createUser() {
+		return User.builder()
+			.nickname(DEFAULT_NICKNAME)
+			.profileImgUrl(DEFAULT_PROFILE_IMG_URL)
+			.provider(PROVIDER_KAKAO)
+			.oauthId(OAUTH_ID)
+			.build();
+	}
+
+	public static UpdateUserRequest getUpdateUserRequest() {
+		return new UpdateUserRequest(
+			"수정된 이름",
+			"https://updatedImg.jpg",
+			"수정돤 자기소개"
+		);
+	}
+
+}
