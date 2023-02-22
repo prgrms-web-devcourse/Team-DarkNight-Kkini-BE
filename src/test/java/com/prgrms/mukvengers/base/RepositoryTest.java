37f1cbe7961973225eea6e8addc78e2877229674
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-23 02:32:17 +0900

    refactor(Store): 테스트 추상 클래스 수정

--- src/test/java/com/prgrms/mukvengers/base/RepositoryTest.java
+++ src/test/java/com/prgrms/mukvengers/base/RepositoryTest.java
@@ -1,11 +1,14 @@
 package com.prgrms.mukvengers.base;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import com.prgrms.mukvengers.domain.store.model.Store;
 import com.prgrms.mukvengers.domain.store.repository.StoreRepository;
 import com.prgrms.mukvengers.global.config.jpa.JpaConfig;
+import com.prgrms.mukvengers.utils.StoreObjectProvider;
 
 @DataJpaTest
 @Import(JpaConfig.class)
@@ -14,4 +17,11 @@ public abstract class RepositoryTest {
 	@Autowired
 	protected StoreRepository storeRepository;
 
+	protected Store savedStore;
+
+	@BeforeEach
+	void setUp() {
+		savedStore = StoreObjectProvider.createStore();
+		storeRepository.save(savedStore);
+	}
 }
--- src/test/java/com/prgrms/mukvengers/domain/store/repository/StoreRepositoryTest.java
+++ src/test/java/com/prgrms/mukvengers/domain/store/repository/StoreRepositoryTest.java
@@ -4,23 +4,13 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.prgrms.mukvengers.base.RepositoryTest;
 import com.prgrms.mukvengers.domain.store.model.Store;
-import com.prgrms.mukvengers.utils.StoreObjectProvider;
 
 class StoreRepositoryTest extends RepositoryTest {
-
-	private Store savedStore;
-
-	@BeforeEach
-	void setUp() {
-		savedStore = StoreObjectProvider.createStore();
-		storeRepository.save(savedStore);
-	}
 
 	@Test
 	@DisplayName("[성공] 맵 api로 받은 가게 아이디로 가게를 조회한다.")
