049d3d28bf93ba9ecebdf8ca641cdb641b302429
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-23 02:23:23 +0900

    chore(Store): static import

--- src/test/java/com/prgrms/mukvengers/domain/store/api/StoreControllerTest.java
+++ src/test/java/com/prgrms/mukvengers/domain/store/api/StoreControllerTest.java
@@ -1,16 +1,16 @@
 package com.prgrms.mukvengers.domain.store.api;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.restdocs.payload.JsonFieldType;
 
 import com.prgrms.mukvengers.base.ControllerTest;
 import com.prgrms.mukvengers.domain.store.dto.request.CreateStoreRequest;
@@ -31,14 +31,14 @@ class StoreControllerTest extends ControllerTest {
 				.content(jsonRequest)
 				.accept(APPLICATION_JSON))
 			.andExpect(status().isCreated())
-			.andExpect(header().string("Location", Matchers.containsString("/api/v1/stores")))
+			.andExpect(header().string("Location", containsString("/api/v1/stores")))
 			.andExpect(redirectedUrlPattern("http://localhost:8080/api/v1/stores/*"))
 			.andDo(print())
 			.andDo(document("store-create",
 				requestFields(
-					fieldWithPath("latitude").type(JsonFieldType.STRING).description("위도"),
-					fieldWithPath("longitude").type(JsonFieldType.STRING).description("경도"),
-					fieldWithPath("mapStoreId").type(JsonFieldType.STRING).description("지도 api 제공 id")
+					fieldWithPath("latitude").type(STRING).description("위도"),
+					fieldWithPath("longitude").type(STRING).description("경도"),
+					fieldWithPath("mapStoreId").type(STRING).description("지도 api 제공 id")
 				)
 			));
 	}
