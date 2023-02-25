5b1139389cdc9a16e0eb0c8eac38c23b7d7656c1
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-25 13:15:35 +0900

    feat(Crew): Category, Status 상수 정의 - 카테고리와 상태 상수값 1개씩 추가 - 상수에 이름을 나타내는 속성읇 부여하고 클라이언트는 이 속성을 서버에 보낸다. - 서버는 이 속성을 통해 상수를 찾는다. - enum 저장은 찾은 enum 상수를 String으로 저장하게 된다.

--- src/main/java/com/prgrms/mukvengers/domain/crew/model/vo/Category.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/model/vo/Category.java
@@ -1,4 +1,24 @@
 package com.prgrms.mukvengers.domain.crew.model.vo;
 
+import java.util.Arrays;
+
 public enum Category {
+	QUIET("조용한");
+
+	private final String name;
+
+	Category(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public static Category getCategory(String name) {
+		return Arrays.stream(Category.values())
+			.filter(a -> a.name.equals(name))
+			.findAny()
+			.orElseThrow();
+	}
 }
--- src/main/java/com/prgrms/mukvengers/domain/crew/model/vo/Status.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/model/vo/Status.java
@@ -1,4 +1,24 @@
 package com.prgrms.mukvengers.domain.crew.model.vo;
 
+import java.util.Arrays;
+
 public enum Status {
+	RECRUITING("모집중");
+
+	private final String status;
+
+	Status(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public static Status getStatus(String status) {
+		return Arrays.stream(Status.values())
+			.filter(a -> a.status.equals(status))
+			.findAny()
+			.orElseThrow();
+	}
 }
