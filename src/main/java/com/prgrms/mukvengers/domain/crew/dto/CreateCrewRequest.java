5a8c7b58ae420d445fe9ea81850aaa90d5386126
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-25 13:16:24 +0900

    refactor(Crew): 어노테이션 변경 - Size -> Min, Max로 변경

--- src/main/java/com/prgrms/mukvengers/domain/crew/dto/CreateCrewRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/dto/CreateCrewRequest.java
@@ -1,15 +1,16 @@
 package com.prgrms.mukvengers.domain.crew.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 public record CreateCrewRequest(
 	@NotNull String mapStoreId,
 	@NotBlank String name,
 	@NotBlank String latitude,
 	@NotBlank String longitude,
-	@Size(min = 2, max = 8) Integer capacity,
+	@Min(value = 2) @Max(value = 8) Integer capacity,
 	@NotBlank String status,
 	@NotBlank String content,
 	@NotBlank String category
