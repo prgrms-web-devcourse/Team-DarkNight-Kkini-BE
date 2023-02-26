bd58d533aa0b07288ea00e3c00fc065c3d404f2a
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-02-26 16:23:16 +0900

    refactor(Crew): Controller 수정 - 명세 작성 - ApiResponse로 변경

--- src/main/java/com/prgrms/mukvengers/domain/crew/api/CrewController.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/api/CrewController.java
@@ -23,6 +23,7 @@ import com.prgrms.mukvengers.domain.crew.dto.request.UpdateStatusRequest;
 import com.prgrms.mukvengers.domain.crew.dto.response.CrewResponses;
 import com.prgrms.mukvengers.domain.crew.dto.response.CrewSliceResponse;
 import com.prgrms.mukvengers.domain.crew.service.CrewService;
+import com.prgrms.mukvengers.global.common.dto.ApiResponse;
 import com.prgrms.mukvengers.global.common.dto.IdResponse;
 import com.prgrms.mukvengers.global.security.jwt.JwtAuthentication;
 
@@ -35,6 +36,14 @@ public class CrewController {
 
 	private final CrewService crewService;
 
+	/**
+	 * <pre>
+	 *     밥 모임 생성
+	 * </pre>
+	 * @param createCrewRequest 밥 모임 정보 DTO
+	 * @param user 유저 정보
+	 * @return status : 201, body : 생성된 밥 모임 조회 redirectUri
+	 */
 	@PostMapping(consumes = APPLICATION_JSON_VALUE)
 	public ResponseEntity<URI> create(
 		@RequestBody @Valid CreateCrewRequest createCrewRequest,
@@ -48,28 +57,53 @@ public class CrewController {
 
 	}
 
+	/**
+	 * <pre>
+	 *     맵 api 가게 아이디로 가게의 밥 모임 조회
+	 * </pre>
+	 * @param mapStoreId 맵 api 가게 아이디
+	 * @param cursorId 마지막으로 조회한 아이디
+	 * @param size 페이지 사이즈
+	 * @return status : 200, body : 해당 가게의 현재 모집 중인 밥 모임 정보
+	 */
 	@GetMapping(value = "/{mapStoreId}", produces = APPLICATION_JSON_VALUE)
-	public ResponseEntity<CrewSliceResponse> findByMapStoreId(
+	public ResponseEntity<ApiResponse<CrewSliceResponse>> findByMapStoreId(
 		@PathVariable String mapStoreId,
 		@RequestParam("cursorId") Long cursorId,
 		@RequestParam("size") Integer size) {
 
 		CrewSliceResponse responses = crewService.findByMapStoreId(mapStoreId, cursorId, size);
 
-		return ResponseEntity.ok(responses);
+		return ResponseEntity.ok().body(new ApiResponse<>(responses));
 
 	}
 
+	/**
+	 * <pre>
+	 *     사용자의 위도, 경도로 특정 거리 안에 있는 밥 모임 조회
+	 * </pre>
+	 * @param x 사용자 위도
+	 * @param y 사용자 경도
+	 * @return status : 200, body : 사용자 위치를 기반으로 특정 거리 안에 있는 밥 모임 정보
+	 */
 	@GetMapping(produces = APPLICATION_JSON_VALUE)
-	public ResponseEntity<CrewResponses> findByLocation(
+	public ResponseEntity<ApiResponse<CrewResponses>> findByLocation(
 		@RequestParam("x") String x,
 		@RequestParam("y") String y
 	) {
 		CrewResponses responses = crewService.findByLocation(x, y);
 
-		return ResponseEntity.ok(responses);
+		return ResponseEntity.ok().body(new ApiResponse<>(responses));
 	}
 
+	/**
+	 * <pre>
+	 *     밥 모임 상태 변경
+	 * </pre>
+	 * @param updateStatusRequest 밤 모임 아이디와, 변경할 상태 정보
+	 * @param user 사용자 정보
+	 * @return status : 200
+	 */
 	@PatchMapping
 	public ResponseEntity<Void> updateStatus(
 		@RequestBody @Valid UpdateStatusRequest updateStatusRequest,
