a2355f48674c774c19dad6cc6df426640f410165
kwonjoosung <kwonjoosung@gmail.com>
2023-02-26 18:31:46 +0900

    feat(OAuth): 로그인 기능 구현
    - OAuthService 구현
     - OAuthProvider를 통해 리소스 서버에서 받아온 데이터를 변환한 후 DB에 이미 회원으로 등록 되어있는지 검증(provider 이름, oauthId를 통해)해 등록된 경우 로그인 아니라면 등록하는 로직 -> 반환은 JWT 토큰으로 했으나 추후에 리팩토링이 필요할 것으로 생각됨
    - UserRepository에 Provider와 OauthId로 찾는 메서드 추가(추후에 DB에 유니크 키로 등록할 필요 있을 듯)

--- src/main/java/com/prgrms/mukvengers/domain/user/repository/UserRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/user/repository/UserRepository.java
@@ -1,9 +1,22 @@
 package com.prgrms.mukvengers.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.prgrms.mukvengers.domain.user.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	@Query("""
+		SELECT u
+		FROM User u
+		WHERE u.provider = :provider
+		and u.oauthId = :oauthId
+		""")
+	Optional<User> findByUserIdByProviderAndOauthId(
+		@Param("provider") String provider,
+		@Param("oauthId") String oauthId);
 }
