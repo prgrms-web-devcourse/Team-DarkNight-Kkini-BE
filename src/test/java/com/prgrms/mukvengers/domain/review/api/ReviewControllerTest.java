00910431a5f6ca4a170b5cb351c135d1bfc3b916
hikarigin99 <hikarigin99@gmail.com>
2023-03-01 16:13:16 +0900

    refactor: document import 수정
    - MockMvcRestDocumentation에서 MockMvcRestDocumentationWrapper로 수정

--- src/test/java/com/prgrms/mukvengers/domain/review/api/ReviewControllerTest.java
+++ src/test/java/com/prgrms/mukvengers/domain/review/api/ReviewControllerTest.java
@@ -1,12 +1,12 @@
 package com.prgrms.mukvengers.domain.review.api;
 
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.*;
 import static com.epages.restdocs.apispec.ResourceDocumentation.*;
 import static com.epages.restdocs.apispec.ResourceSnippetParameters.*;
 import static com.prgrms.mukvengers.utils.CrewObjectProvider.*;
 import static com.prgrms.mukvengers.utils.ReviewObjectProvider.*;
 import static com.prgrms.mukvengers.utils.UserObjectProvider.*;
 import static org.springframework.http.MediaType.*;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -79,8 +79,9 @@ class ReviewControllerTest extends ControllerTest {
 				resource(
 					builder()
 						.tag(REVIEW)
+						.summary("방장에 대한 리뷰 생성")
 						.requestFields(
-							fieldWithPath("revieweeId").description("방장의 아이디"),
+							fieldWithPath("leaderId").description("방장의 아이디"),
 							fieldWithPath("content").description("추가로 작성하고 싶은 내용"),
 							fieldWithPath("mannerPoint").description("방장에 대한 매너 온도"),
 							fieldWithPath("tastePoint").description("방장에 대한 맛잘알 점수"))
@@ -107,6 +108,7 @@ class ReviewControllerTest extends ControllerTest {
 				resource(
 					builder()
 						.tag(REVIEW)
+						.summary("방장이 아닌 밥모임원에 대한 리뷰 생성")
 						.requestFields(
 							fieldWithPath("revieweeId").description("리뷰이 아이디"),
 							fieldWithPath("content").description("추가로 작성하고 싶은 내용"),
@@ -130,36 +132,42 @@ class ReviewControllerTest extends ControllerTest {
 			.andExpect(status().isOk())
 			.andDo(print())
 			.andDo(document("singleReview",
-				responseFields(
-					fieldWithPath("data.reviewer.id").description("리뷰어의 아이디"),
-					fieldWithPath("data.reviewer.nickname").description("리뷰어의 닉네임"),
-					fieldWithPath("data.reviewer.profileImgUrl").description("리뷰어의 프로필 이미지 주소"),
-					fieldWithPath("data.reviewer.introduction").description("리뷰어의 한줄 소개"),
-					fieldWithPath("data.reviewer.leaderCount").description("리뷰어의 리더 횟수"),
-					fieldWithPath("data.reviewer.crewCount").description("리뷰어의 밥모임 참여한 카운트"),
-					fieldWithPath("data.reviewer.tasteScore").description("리뷰어의 맛잘알 점수"),
-					fieldWithPath("data.reviewer.mannerScore").description("리뷰어의 매너 점수"),
+				resource(
+					builder()
+						.tag(REVIEW)
+						.summary("리뷰 단건 조회")
+						.responseFields(
+							fieldWithPath("data.reviewer.id").description("리뷰어의 아이디"),
+							fieldWithPath("data.reviewer.nickname").description("리뷰어의 닉네임"),
+							fieldWithPath("data.reviewer.profileImgUrl").description("리뷰어의 프로필 이미지 주소"),
+							fieldWithPath("data.reviewer.introduction").description("리뷰어의 한줄 소개"),
+							fieldWithPath("data.reviewer.leaderCount").description("리뷰어의 리더 횟수"),
+							fieldWithPath("data.reviewer.crewCount").description("리뷰어의 밥모임 참여한 카운트"),
+							fieldWithPath("data.reviewer.tasteScore").description("리뷰어의 맛잘알 점수"),
+							fieldWithPath("data.reviewer.mannerScore").description("리뷰어의 매너 점수"),
 
-					fieldWithPath("data.reviewee.id").type(NUMBER).description("리뷰이의 아이디"),
-					fieldWithPath("data.reviewee.nickname").type(STRING).description("리뷰이의 닉네임"),
-					fieldWithPath("data.reviewee.profileImgUrl").type(STRING).description("리뷰이의 프로필 이미지 주소"),
-					fieldWithPath("data.reviewee.introduction").type(STRING).description("리뷰이의 한줄 소개"),
-					fieldWithPath("data.reviewee.leaderCount").type(NUMBER).description("리뷰이의 리더 횟수"),
-					fieldWithPath("data.reviewee.crewCount").type(NUMBER).description("리뷰이의 밥모임 참여한 횟수"),
-					fieldWithPath("data.reviewee.tasteScore").type(NUMBER).description("리뷰이의 맛잘알 점수"),
-					fieldWithPath("data.reviewee.mannerScore").type(NUMBER).description("리뷰이의 매너 점수"),
+							fieldWithPath("data.reviewee.id").type(NUMBER).description("리뷰이의 아이디"),
+							fieldWithPath("data.reviewee.nickname").type(STRING).description("리뷰이의 닉네임"),
+							fieldWithPath("data.reviewee.profileImgUrl").type(STRING).description("리뷰이의 프로필 이미지 주소"),
+							fieldWithPath("data.reviewee.introduction").type(STRING).description("리뷰이의 한줄 소개"),
+							fieldWithPath("data.reviewee.leaderCount").type(NUMBER).description("리뷰이의 리더 횟수"),
+							fieldWithPath("data.reviewee.crewCount").type(NUMBER).description("리뷰이의 밥모임 참여한 횟수"),
+							fieldWithPath("data.reviewee.tasteScore").type(NUMBER).description("리뷰이의 맛잘알 점수"),
+							fieldWithPath("data.reviewee.mannerScore").type(NUMBER).description("리뷰이의 매너 점수"),
 
-					fieldWithPath("data.store.id").type(NUMBER).description("가게의 아이디"),
-					fieldWithPath("data.store.latitude").type(STRING).description("가게의 위도"),
-					fieldWithPath("data.store.longitude").type(STRING).description("가게의 경도"),
-					fieldWithPath("data.store.mapStoreId").type(STRING).description("지도 api 제공 id"),
+							fieldWithPath("data.store.id").type(NUMBER).description("가게의 아이디"),
+							fieldWithPath("data.store.latitude").type(STRING).description("가게의 위도"),
+							fieldWithPath("data.store.longitude").type(STRING).description("가게의 경도"),
+							fieldWithPath("data.store.mapStoreId").type(STRING).description("지도 api 제공 id"),
 
-					fieldWithPath("data.crewName").type(STRING).description("리뷰하고자 하는 밥모임 이름"),
-					fieldWithPath("data.promiseTime").type(ARRAY).description("리뷰하고자 하는 밥모임 약속 시간"),
-					fieldWithPath("data.content").type(STRING).description("리뷰하고자 하는 밥모임 간단 한줄 리뷰 "),
-					fieldWithPath("data.mannerPoint").type(NUMBER).description("리뷰하고자 하는 밥모임 매너 온도"),
-					fieldWithPath("data.tastePoint").type(NUMBER).description("리뷰하고자 하는 밥모임 맛잘알 점수")
-					)
+							fieldWithPath("data.crewName").type(STRING).description("리뷰하고자 하는 밥모임 이름"),
+							fieldWithPath("data.promiseTime").type(ARRAY).description("리뷰하고자 하는 밥모임 약속 시간"),
+							fieldWithPath("data.content").type(STRING).description("리뷰하고자 하는 밥모임 간단 한줄 리뷰 "),
+							fieldWithPath("data.mannerPoint").type(NUMBER).description("리뷰하고자 하는 밥모임 매너 온도"),
+							fieldWithPath("data.tastePoint").type(NUMBER).description("리뷰하고자 하는 밥모임 맛잘알 점수")
+						)
+						.build()
+				)
 			));
 	}
 
