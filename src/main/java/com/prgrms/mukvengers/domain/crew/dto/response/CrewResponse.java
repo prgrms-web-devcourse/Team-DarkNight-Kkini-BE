baf109a96f432cbe072f8da696ca9b1d86b2a67e
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-04 01:00:40 +0900

    refactor(Crew): 모임 조회 반환값에서 가게정보, 위경도 정보 삭제

--- src/main/java/com/prgrms/mukvengers/domain/crew/dto/response/CrewResponse.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/dto/response/CrewResponse.java
@@ -2,18 +2,13 @@ package com.prgrms.mukvengers.domain.crew.dto.response;
 
 import java.time.LocalDateTime;
 
-import com.prgrms.mukvengers.domain.store.dto.response.StoreResponse;
-
 public record CrewResponse(
 	Long id,
-	StoreResponse store,
 	String name,
-	String longitude,
-	String latitude,
 	Integer capacity,
-	LocalDateTime promiseTime,
 	String status,
 	String content,
-	String category
+	String category,
+	LocalDateTime promiseTime
 ) {
 }
