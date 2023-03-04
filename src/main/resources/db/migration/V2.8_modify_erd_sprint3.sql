4131b95a02e6034cc4ad8ab366e8aa87180a5de1
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-05 03:55:01 +0900

    refactor: comment 테이블 수정 취소 - 댓글 테이블은 채팅기능을 구현하는 팀원이 수정하는게 맞다고 생각하여 수정을 취소하였습니다.

--- src/main/resources/db/migration/V2.8_modify_erd_sprint3.sql
+++ src/main/resources/db/migration/V2.8_modify_erd_sprint3.sql
@@ -1,18 +1,3 @@
-DROP TABLE comment;
-
-CREATE TABLE comment
-(
-    id         bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    crew_id    bigint       NOT NULL,
-    content    varchar(255) NOT NULL,
-    created_at dateTime     NOT NULL DEFAULT now(),
-    updated_at dateTime     NOT NULL DEFAULT now(),
-    deleted    boolean      NOT NULL DEFAULT false,
-
-    FOREIGN KEY fk_comment_leader_id (crew_id) REFERENCES crew (id)
-);
-
-
 CREATE TABLE proposal
 (
     id         bigint       NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -26,6 +11,6 @@ CREATE TABLE proposal
     deleted    boolean      NOT NULL DEFAULT false,
 
     FOREIGN KEY fk_proposal_user_id (user_id) REFERENCES users (id),
-    FOREIGN KEY fk_proposal_user_id (leader_id) REFERENCES users (id),
+    FOREIGN KEY fk_proposal_leader_id (leader_id) REFERENCES users (id),
     FOREIGN KEY fk_proposal_crew_id (crew_id) REFERENCES crew (id)
 );
