1c53c016b9e2258dda5133000ed99f54d8f7cc92
hikarigin99 <hikarigin99@gmail.com>
2023-03-04 22:51:06 +0900

    feat: ObjectProvider 수정 및 추가
    - reviewObjectProvider: store, crewName 필드 삭제
    - ReviewObjectProvider: crew 필드 추가
    - CrewMemberObjectProvider 추가
    - UserObjectProvider: 파라미터로 OAuth ID 받는 User 생성 메서드 추가

--- src/main/java/com/prgrms/mukvengers/domain/review/dto/request/CreateLeaderReviewRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/review/dto/request/CreateLeaderReviewRequest.java
@@ -3,7 +3,7 @@ package com.prgrms.mukvengers.domain.review.dto.request;
 import javax.validation.constraints.NotNull;
 
 public record CreateLeaderReviewRequest(
-	@NotNull Long leaderId, // reviewee : 방장
+	@NotNull Long leaderId,
 	@NotNull String content,
 	@NotNull Integer mannerPoint,
 	@NotNull Integer tastePoint
