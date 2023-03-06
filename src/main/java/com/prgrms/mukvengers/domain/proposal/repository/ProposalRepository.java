2b91a35d275fc3cbc38c3a9d602aa7a32cb43ad5
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-07 03:52:14 +0900

    feat(Proposal): 사용자 아이디를 leader_id 컬럼과 비교하여 일치하는 신청서 조회 기능

--- src/main/java/com/prgrms/mukvengers/domain/proposal/repository/ProposalRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/proposal/repository/ProposalRepository.java
@@ -1,8 +1,14 @@
 package com.prgrms.mukvengers.domain.proposal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.prgrms.mukvengers.domain.proposal.model.Proposal;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
+
+	List<Proposal> findAllByLeaderId(@Param("userId") Long userId);
+
 }
