f0d5b074fed7885ec633d34c0bbed0d417c8f79b
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2023-03-06 16:31:18 +0900

    fix(CORS): allowedOriginPatterns("/**") 로 다시 변경
    - When allowCredentials is true, allowedOrigins cannot contain the special value "*" since that cannot be set on the "Access-Control-Allow-Origin" response header. To allow credentials to a set of origins, list them explicitly or consider using "allowedOriginPatterns" instead.

--- src/main/java/com/prgrms/mukvengers/global/config/web/WebConfig.java
+++ src/main/java/com/prgrms/mukvengers/global/config/web/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("/**")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowCredentials(true);
 	}
