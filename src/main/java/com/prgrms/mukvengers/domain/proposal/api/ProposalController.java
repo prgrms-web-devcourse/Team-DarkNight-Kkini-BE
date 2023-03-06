29db83cb551f0b0188d9a58f1290a4a29305926c
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-07 03:54:27 +0900

    feat(Proposal): 사용자가 방장인 모임의 신청서 조회 기능 구현 - 사용자의 아이디와 신청서 테이블의 leader_id 컬럼과 비교하여 모든 신청서를 조회한다. - 추후 페이지네이션으로 변경될 수 있습니다.

---
+++ src/main/java/com/prgrms/mukvengers/domain/proposal/api/ProposalController.java
@@ -0,0 +1,42 @@
+package com.prgrms.mukvengers.domain.proposal.api;
+
+import static org.springframework.http.MediaType.*;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prgrms.mukvengers.domain.proposal.dto.response.ProposalResponses;
+import com.prgrms.mukvengers.domain.proposal.service.ProposalService;
+import com.prgrms.mukvengers.global.common.dto.ApiResponse;
+import com.prgrms.mukvengers.global.security.jwt.JwtAuthentication;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ProposalController {
+
+	private final ProposalService proposalService;
+
+	/**
+	 * <pre>
+	 *     사용자가 방장인 모임의 신청서를 모두 조회합니다.
+	 * </pre>
+	 * @param user 사용자 정보
+	 * @return status : 200, body : 조회된 모든 신청서 데이터
+	 */
+	@GetMapping(value = "/proposals/leader", produces = APPLICATION_JSON_VALUE)
+	public ResponseEntity<ApiResponse<ProposalResponses>> getProposalsByLeaderId
+	(
+		@AuthenticationPrincipal JwtAuthentication user
+	) {
+		ProposalResponses responses = proposalService.getProposalsByLeaderId(user.id());
+
+		return ResponseEntity.ok().body(new ApiResponse<>(responses));
+	}
+
+}
