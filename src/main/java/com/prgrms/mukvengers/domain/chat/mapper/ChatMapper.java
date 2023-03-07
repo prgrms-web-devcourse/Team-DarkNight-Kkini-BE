febb2885b9fa28cd8730aa2fa68a5c5f0a0f683e
김기서 <5rne@naver.com>
2023-03-08 04:03:35 +0900

    feat(Chat): ChatMapper, ChatDto(response) 생성

---
+++ src/main/java/com/prgrms/mukvengers/domain/chat/mapper/ChatMapper.java
@@ -0,0 +1,25 @@
+package com.prgrms.mukvengers.domain.chat.mapper;
+
+import static org.mapstruct.ReportingPolicy.*;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse;
+import com.prgrms.mukvengers.domain.chat.model.Chat;
+import com.prgrms.mukvengers.global.websocket.ChatMessage;
+
+@Mapper(componentModel = "spring", unmappedSourcePolicy = IGNORE)
+public interface ChatMapper {
+
+	@Mapping(target = "content", source = "chatMessage.content")
+	@Mapping(target = "sender", source = "chatMessage.sender")
+	@Mapping(target = "userId", source = "chatMessage.userId")
+	@Mapping(target = "crewId", source = "crewId")
+	Chat toChat(ChatMessage chatMessage, Long crewId);
+
+	@Mapping(target = "content", source = "content")
+	@Mapping(target = "sender", source = "sender")
+	@Mapping(target = "createdAt", source = "createdAt")
+	ChatResponse toChatResponse(Chat chat);
+}
