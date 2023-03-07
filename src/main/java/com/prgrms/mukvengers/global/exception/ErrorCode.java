2aff8f612adeca6d4d144ed7ef5a93bf54c2b156
kwonjoosung <kwonjoosung@gmail.com>
2023-03-08 02:24:34 +0900

    refactor(Token): 리프레쉬 토큰에서 발생할 수 있는 예외 추가
    - NOT_FOUND_REFRESH_TOKEN: 리프레쉬 토큰이 없거나, 만료, 일치하지 않는 경우, 등 에서 발생
    - NOT_FOUND_COOKIE: 쿠키에 refreshtoken에 해당하는 값이 없을 때 발생
    - 해당하는 예외 객체 및 코드 추가

--- src/main/java/com/prgrms/mukvengers/global/exception/ErrorCode.java
+++ src/main/java/com/prgrms/mukvengers/global/exception/ErrorCode.java
@@ -39,7 +39,9 @@ public enum ErrorCode {
 	// Auth
 	INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "A001", "유효하지 않은 토큰입니다."),
 	EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "만료된 토큰입니다."),
-	NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "A002", "로그인이 필요합니다.");
+	NOT_FOUND_TOKEN(HttpStatus.UNAUTHORIZED, "A003", "로그인이 필요합니다."),
+	NOT_FOUND_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "A004", "유효하지 않은 리프레쉬 토큰입니다."),
+	NOT_FOUND_COOKIE(HttpStatus.UNAUTHORIZED, "A005", "쿠키를 찾을 수 없습니다.");
 
 	private final HttpStatus status;
 	private final String code;
