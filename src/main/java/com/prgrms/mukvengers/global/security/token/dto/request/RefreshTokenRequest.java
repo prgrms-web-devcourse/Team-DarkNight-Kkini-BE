d62c56e9c3ed7800d43dd65bff649ba9d0709537
kwonjoosung <kwonjoosung@gmail.com>
2023-03-08 02:26:48 +0900

    refactor(Token): 불필요한 객체 제거
    - Token -> RefreshToken 으로 통합
    - RefreshTokenRequest: Request를 쿠키로 받기 때문에 불필요함

--- src/main/java/com/prgrms/mukvengers/global/security/token/dto/request/RefreshTokenRequest.java
+++
@@ -1,8 +0,0 @@
-package com.prgrms.mukvengers.global.security.token.dto.request;
-
-import javax.validation.constraints.NotBlank;
-
-public record RefreshTokenRequest(
-	@NotBlank String refreshToken
-) {
-}
