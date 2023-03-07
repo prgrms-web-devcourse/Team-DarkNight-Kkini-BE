0eb2963ca6c50b512cbb34eebee18505e6a6c7e1
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-03-07 13:33:12 +0900

    refactor(Crew): 모임 생성 DTO 수정 - 모임을 생성할때 가게정보도 함께 받아 가게가 저장되어 있지 않을 경우 가게를 저장하기 위해 가제 정보도 같이 받도록 수정하였습니다.

--- src/main/java/com/prgrms/mukvengers/domain/crew/dto/request/CreateCrewRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/dto/request/CreateCrewRequest.java
@@ -7,11 +7,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.prgrms.mukvengers.domain.store.dto.request.CreateStoreRequest;
+
 public record CreateCrewRequest(
-	@NotBlank String placeId,
+
+	@NotNull CreateStoreRequest createStoreRequest,
 	@NotBlank String name,
-	@NotBlank String longitude,
-	@NotBlank String latitude,
 	@NotNull LocalDateTime promiseTime,
 	@Min(value = 2) @Max(value = 8) Integer capacity,
 	@NotBlank String content,
