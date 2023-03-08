0ac492d0bf055d3fb76c8b471c81cf0be04c394d
hikarigin99 <hikarigin99@gmail.com>
2023-03-08 17:30:12 +0900

    test: 신청서(Proposal) 생성 시 검증하는 서비스 테스트

--- src/test/java/com/prgrms/mukvengers/domain/proposal/service/ProposalServiceImplTest.java
+++ src/test/java/com/prgrms/mukvengers/domain/proposal/service/ProposalServiceImplTest.java
@@ -1,11 +1,13 @@
 package com.prgrms.mukvengers.domain.proposal.service;
 
+import static com.prgrms.mukvengers.domain.proposal.service.ProposalServiceImpl.*;
 import static com.prgrms.mukvengers.utils.CrewObjectProvider.*;
 import static com.prgrms.mukvengers.utils.ProposalObjectProvider.*;
 import static com.prgrms.mukvengers.utils.UserObjectProvider.*;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,139 @@ import org.junit.jupiter.api.Test;
 import com.prgrms.mukvengers.base.ServiceTest;
 import com.prgrms.mukvengers.domain.crew.model.Crew;
 import com.prgrms.mukvengers.domain.crew.model.vo.CrewStatus;
+import com.prgrms.mukvengers.domain.crewmember.model.CrewMember;
+import com.prgrms.mukvengers.domain.crewmember.model.vo.CrewMemberRole;
+import com.prgrms.mukvengers.domain.proposal.dto.request.CreateProposalRequest;
 import com.prgrms.mukvengers.domain.proposal.dto.response.ProposalResponses;
 import com.prgrms.mukvengers.domain.proposal.model.Proposal;
 import com.prgrms.mukvengers.domain.user.model.User;
+import com.prgrms.mukvengers.global.common.dto.IdResponse;
+import com.prgrms.mukvengers.utils.CrewMemberObjectProvider;
+import com.prgrms.mukvengers.utils.ProposalObjectProvider;
 
 class ProposalServiceImplTest extends ServiceTest {
+
+	@Test
+	@DisplayName("[성공] 사용자는 신청서를 작성할 수 있다.")
+	void createProposal_success() {
+
+		//given
+		User createUser = createUser("12121212");
+		User leader = userRepository.save(createUser);
+
+		Crew createCrew = createCrew(savedStore, CrewStatus.RECRUITING);
+		Crew crew = crewRepository.save(createCrew);
+
+		CreateProposalRequest proposalRequest = ProposalObjectProvider.createProposalRequest(leader.getId());
+
+		// when
+		IdResponse response = proposalService.create(proposalRequest, savedUserId, crew.getId());
+
+		// then
+		Optional<Proposal> findProposal = proposalRepository.findById(response.id());
+		assertThat(findProposal).isPresent();
+		assertThat(findProposal.get())
+			.hasFieldOrPropertyWithValue("user", savedUser)
+			.hasFieldOrPropertyWithValue("leaderId", leader.getId())
+			.hasFieldOrPropertyWithValue("crewId", crew.getId())
+			.hasFieldOrPropertyWithValue("content", proposalRequest.content());
+	}
+
+	@Test
+	@DisplayName("[실패] 모집 정원이 다 찬 밥모임에는 신청서를 작성할 수 없다.")
+	void createProposal_fail_countOverCapacity() {
+
+		//given
+		User createUser = createUser("12121212");
+		User leader = userRepository.save(createUser);
+
+		Crew createCrew = createCrew(savedStore, CrewStatus.RECRUITING);
+		Crew crew = crewRepository.save(createCrew);
+
+		CrewMember createCrewMember = CrewMemberObjectProvider.createCrewMember(leader.getId(), crew, CrewMemberRole.LEADER);
+		crewMemberRepository.save(createCrewMember);
+
+		List<CrewMember> crewMembers = CrewMemberObjectProvider.createCrewMembers(savedUserId, crew, CrewMemberRole.MEMBER,
+			crew.getCapacity());
+
+		crewMemberRepository.saveAll(crewMembers);
+
+		CreateProposalRequest proposalRequest = ProposalObjectProvider.createProposalRequest(leader.getId());
+
+		// when & then
+		assertThatThrownBy
+			(
+				() -> proposalService.create(proposalRequest, savedUserId, crew.getId())
+			)
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(CREW_MEMBER_COUNT_OVER_CAPACITY_EXCEPTION_MESSAGE);
+	}
+
+	@Test
+	@DisplayName("[실패] 해당 밥모임에 강퇴된 사용자라면 신청서를 작성할 수 없다.")
+	void createProposal_fail_blockedUser() {
+
+		//given
+		Crew createCrew = createCrew(savedStore, CrewStatus.RECRUITING);
+		Crew crew = crewRepository.save(createCrew);
+
+		CrewMember createCrewMember = CrewMemberObjectProvider.createCrewMember(savedUserId, crew, CrewMemberRole.BLOCKED);
+		crewMemberRepository.save(createCrewMember);
+
+		CreateProposalRequest proposalRequest = ProposalObjectProvider.createProposalRequest(savedUserId);
+
+		// when & then
+		assertThatThrownBy
+			(
+				() -> proposalService.create(proposalRequest, savedUserId, crew.getId())
+			)
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(DISMISSED_USER_EXCEPTION_MESSAGE);
+	}
+
+	@Test
+	@DisplayName("[실패] 사용자가 해당 밥모임의 리더라면 신청서를 작성할 수 없다.")
+	void createProposal_fail_LeaderUser() {
+
+		//given
+		Crew createCrew = createCrew(savedStore, CrewStatus.RECRUITING);
+		Crew crew = crewRepository.save(createCrew);
+
+		CrewMember createCrewMember = CrewMemberObjectProvider.createCrewMember(savedUserId, crew, CrewMemberRole.LEADER);
+		crewMemberRepository.save(createCrewMember);
+
+		CreateProposalRequest proposalRequest = ProposalObjectProvider.createProposalRequest(savedUserId);
+
+		// when & then
+		assertThatThrownBy
+			(
+				() -> proposalService.create(proposalRequest, savedUserId, crew.getId())
+			)
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(LEADER_USER_EXCEPTION_MESSAGE);
+	}
+
+	@Test
+	@DisplayName("[실패] 사용자가 이미 해당 밥모임에 참여자라면 신청서를 작성할 수 없다.")
+	void createProposal_fail_DuplicatedUser() {
+
+		//given
+		Crew createCrew = createCrew(savedStore, CrewStatus.RECRUITING);
+		Crew crew = crewRepository.save(createCrew);
+
+		CrewMember createCrewMember = CrewMemberObjectProvider.createCrewMember(savedUserId, crew, CrewMemberRole.MEMBER);
+		crewMemberRepository.save(createCrewMember);
+
+		CreateProposalRequest proposalRequest = ProposalObjectProvider.createProposalRequest(savedUserId);
+
+		// when & then
+		assertThatThrownBy
+			(
+				() -> proposalService.create(proposalRequest, savedUserId, crew.getId())
+			)
+			.isInstanceOf(IllegalStateException.class)
+			.hasMessageContaining(DUPLICATE_USER_EXCEPTION_MESSAGE);
+	}
 
 	@Test
 	@DisplayName("[성공] 사용자가 방장인 모임의 모든 신청서를 조회한다.")
@@ -41,7 +171,7 @@ class ProposalServiceImplTest extends ServiceTest {
 	}
 
 	@Test
-	@DisplayName("[성공] 사용자가 방장인 아니고 참여자인 모임의 신청서를 모두 조회합니다.")
+	@DisplayName("[성공] 사용자가 방장인 아니고 참여자인 밥모임의 신청서를 모두 조회합니다.")
 	void getProposalsByMemberId_success() {
 
 		//given
