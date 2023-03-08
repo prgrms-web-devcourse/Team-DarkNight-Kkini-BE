3c49c8f7bda32ba7f104a2b5c55cd4fc2384c82a
hikarigin99 <hikarigin99@gmail.com>
2023-03-08 15:00:29 +0900

    refactor: Review Content null validate 제거
    - 기존 Request Dto에서 @NotNull validation을 @Nullable로 변경

--- src/main/java/com/prgrms/mukvengers/domain/review/dto/request/CreateLeaderReviewRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/review/dto/request/CreateLeaderReviewRequest.java
@@ -2,9 +2,11 @@ package com.prgrms.mukvengers.domain.review.dto.request;
 
 import javax.validation.constraints.NotNull;
 
+import org.springframework.lang.Nullable;
+
 public record CreateLeaderReviewRequest(
 	@NotNull Long leaderId,
-	@NotNull String content,
+	@Nullable String content,
 	@NotNull Integer mannerScore,
 	@NotNull Integer tasteScore
 ) {
