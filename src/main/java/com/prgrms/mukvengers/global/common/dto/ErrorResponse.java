77c697936aedfb6ff353a7b4a47c1ab83b6c0c13
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2023-03-08 18:31:06 +0900

    fix(Response): ErrorResponse code랑 message 순서 바뀐 문제 수정
    - code에 message가 바인딩되고 message에 code가 바인딩 되는 문제

--- src/main/java/com/prgrms/mukvengers/global/common/dto/ErrorResponse.java
+++ src/main/java/com/prgrms/mukvengers/global/common/dto/ErrorResponse.java
@@ -8,7 +8,7 @@ public record ErrorResponse(
 ) {
 
 	public static ErrorResponse of(ErrorCode code) {
-		return new ErrorResponse(code.getMessage(), code.getCode());
+		return new ErrorResponse(code.getCode(), code.getMessage());
 	}
 
 }
