7ce3cf72e69f8173c822915a4d5c73e7609c4077
kwonjoosung <kwonjoosung@gmail.com>
2023-03-10 00:33:47 +0900

    refactor(WebSocket): StompHandler 수정
    - StompHandler에서 연결시, 구독시, 연결해제 시에 동작로직 추가
    - 유저 인증
    - 유저가 모임원인지 검증(채팅방에 참가할 수 있는 자격이 있는지 확인함)

--- src/main/java/com/prgrms/mukvengers/global/websocket/StompHandler.java
+++ src/main/java/com/prgrms/mukvengers/global/websocket/StompHandler.java
@@ -1,5 +1,6 @@
 package com.prgrms.mukvengers.global.websocket;
 
+import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.messaging.Message;
@@ -9,8 +10,15 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+import com.prgrms.mukvengers.domain.crewmember.repository.CrewMemberRepository;
+import com.prgrms.mukvengers.domain.user.exception.UserNotFoundException;
+import com.prgrms.mukvengers.domain.user.model.User;
+import com.prgrms.mukvengers.domain.user.repository.UserRepository;
 import com.prgrms.mukvengers.global.security.jwt.JwtTokenProvider;
+import com.prgrms.mukvengers.global.utils.ExtractUtil;
+import com.prgrms.mukvengers.global.websocket.exception.WebSocketException;
 
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,28 +26,101 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @RequiredArgsConstructor
 public class StompHandler implements ChannelInterceptor {
-	private final JwtTokenProvider jwtTokenProvider;
 
+	public static final String USER_ID = "userId";
+	public static final String CREW_ID = "crewId";
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final UserRepository userRepository;
+	private final CrewMemberRepository crewMemberRepository;
+
+	// websocket을 통해 들어온 요청이 처리 되기전 실행된다.
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
 		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-		log.error("message:" + message);
-		log.error("헤더 : " + message.getHeaders());
-		log.error("토큰" + accessor.getNativeHeader("Authorization"));
-		if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-			jwtTokenProvider.validateToken(
-				extractTokenByHeader(accessor.getFirstNativeHeader("Authorization")));
+
+		if (StompCommand.CONNECT.equals(accessor.getCommand())) { // websocket 연결요청 -> JWT 인증
+
+			User user = getUserByAuthorizationHeader(
+				accessor.getFirstNativeHeader("Authorization"));
+
+			setValue(accessor, USER_ID, user.getId());
+			setValue(accessor, "username", user.getNickname());
+			setValue(accessor, "profileImgUrl", user.getProfileImgUrl());
+
+			log.error("헤더 : " + message.getHeaders());
+			log.error("message:" + message);
+
+		} else if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) { // 채팅룸 구독요청(진입) -> CrewMember인지 검증
+
+			validateUserInCrew(accessor);
+
+		} else if (StompCommand.DISCONNECT == accessor.getCommand()) { // Websocket 연결 종료
+			// 연결이 종료된 클라이언트 sesssionId로 채팅방 id를 얻는다.
+			Long userId = (Long)getValue(accessor, USER_ID);
+			Long crewId = (Long)getValue(accessor, CREW_ID);
+			log.info("DISCONNECTED crewId : {}, userId : {}", crewId, userId);
 		}
+
 		return message;
 	}
 
-	private String extractTokenByHeader(String authHeaderValue) {
+	private User getUserByAuthorizationHeader(String authHeaderValue) {
 
-		if (Objects.nonNull(authHeaderValue) &&
-			authHeaderValue.toLowerCase().startsWith("Bearer" .toLowerCase())) {
-			return authHeaderValue.substring("Bearer" .length()).trim();
+		String accessToken = getTokenByAuthorizationHeader(authHeaderValue);
+
+		Claims claims = jwtTokenProvider.getClaims(accessToken);
+		Long userId = claims.get(USER_ID, Long.class);
+
+		return userRepository.findById(userId)
+			.orElseThrow(() -> new UserNotFoundException(userId));
+	}
+
+	private String getTokenByAuthorizationHeader(String authHeaderValue) {
+
+		if (Objects.isNull(authHeaderValue) || authHeaderValue.isBlank()) {
+			throw new WebSocketException();
 		}
 
-		throw new IllegalArgumentException("헤더에 값이 없습니다");
+		String accessToken = ExtractUtil.extractToken(authHeaderValue);
+		jwtTokenProvider.validateToken(accessToken);
+
+		return accessToken;
 	}
+
+	private void validateUserInCrew(StompHeaderAccessor accessor) {
+		Long userId = (Long)getValue(accessor, USER_ID);
+		Long crewId = (Long)getValue(accessor, CREW_ID);
+
+		crewMemberRepository.findCrewMemberByCrewIdAndUserId(crewId, userId)
+			.orElseThrow(WebSocketException::new);
+	}
+
+	private Object getValue(StompHeaderAccessor accessor, String valueName) {
+		Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+
+		if (Objects.isNull(sessionAttributes)) {
+			throw new WebSocketException();
+		}
+
+		Object value = sessionAttributes.get(valueName);
+
+		if (Objects.isNull(value)) {
+			throw new WebSocketException();
+		}
+
+		return value;
+	}
+
+	private void setValue(StompHeaderAccessor accessor, String key, Object value) {
+		Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+
+		if (Objects.isNull(sessionAttributes)) {
+			throw new WebSocketException();
+		}
+
+		sessionAttributes.put(key, value);
+	}
+
 }
