6193401d978e6953a67329fcf6196e32e75d9e39
kwonjoosung <kwonjoosung@gmail.com>
2023-03-10 18:23:30 +0900

    fix(Config): crewMember 및 Review 더미 추가
    - 기존 리뷰 정보에 빈 sql 문을 올림

---
+++ src/main/resources/db/migration/R__review_and_crew_member_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO crew_member(user_id, crew_id, crew_member_role)
+VALUES (13, 1, 'LEADER');
+
+INSERT INTO review(reviewer_id, reviewee_id, crew_id, promise_time, content, manner_score, taste_score)
+VALUES (13, 1, 1, '2023-03-08 13:00:00', '맛잘알 인정합니다', 5, 5);
