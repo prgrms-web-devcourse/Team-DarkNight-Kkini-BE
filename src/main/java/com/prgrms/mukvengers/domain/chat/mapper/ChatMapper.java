2d1c198a57d806be9b60dedd5befd5facc2f4c26
kwonjoosung <kwonjoosung@gmail.com>
2023-03-11 22:49:19 +0900

    refactor(Chat): ChatMapper에 mapping 메서드 추가
    - chatResponse으로 매핑

--- src/main/java/com/prgrms/mukvengers/domain/chat/mapper/ChatMapper.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/mapper/ChatMapper.java
@@ -5,21 +5,23 @@ import static org.mapstruct.ReportingPolicy.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import com.prgrms.mukvengers.domain.chat.dto.request.ChatRequest;
 import com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse;
 import com.prgrms.mukvengers.domain.chat.model.Chat;
-import com.prgrms.mukvengers.domain.chat.model.ChatMessage;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = IGNORE)
 public interface ChatMapper {
 
-	@Mapping(target = "content", source = "chatMessage.content")
-	@Mapping(target = "sender", source = "chatMessage.sender")
-	@Mapping(target = "userId", source = "chatMessage.userId")
+	@Mapping(target = "content", source = "chatRequest.content")
+	@Mapping(target = "userId", source = "chatRequest.userId")
 	@Mapping(target = "crewId", source = "crewId")
-	Chat toChat(ChatMessage chatMessage, Long crewId);
+	Chat toChat(ChatRequest chatRequest, Long crewId);
 
-	@Mapping(target = "content", source = "content")
-	@Mapping(target = "sender", source = "sender")
-	@Mapping(target = "createdAt", source = "createdAt")
-	ChatResponse toChatResponse(Chat chat);
+	@Mapping(target = "type", source = "chat.type")
+	@Mapping(target = "userId", source = "chat.userId")
+	@Mapping(target = "username", source = "username")
+	@Mapping(target = "profileImgUrl", source = "profileImgUrl")
+	@Mapping(target = "createdAt", source = "chat.createdAt")
+	@Mapping(target = "content", source = "chat.content")
+	ChatResponse toChatResponse(Chat chat, String username, String profileImgUrl);
 }
