76695e14728ad22e628cdd6c69f9e3c00422edb9
kwonjoosung <kwonjoosung@gmail.com>
2023-03-11 22:30:30 +0900

    refactor(Chat): DTO 추가 생성 및 기존 객체 필드 변경
    - request, response 타입 구분
    - request에는 최소한의 정보만 담을 수 있도록 함
    - response에 유저이름과 프로필 이미지 정보, 생성 시간 추가
    - MessageType 패키지 변경

--- src/main/java/com/prgrms/mukvengers/domain/chat/dto/response/ChatResponse.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/dto/response/ChatResponse.java
@@ -2,9 +2,17 @@ package com.prgrms.mukvengers.domain.chat.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.prgrms.mukvengers.domain.chat.dto.request.MessageType;
+
+import lombok.Builder;
+
+@Builder
 public record ChatResponse(
-	String content,
-	String sender,
-	LocalDateTime createdAt
+	MessageType type,
+	Long userId,
+	String username,
+	String profileImgUrl,
+	LocalDateTime createdAt,
+	String content
 ) {
 }
--- src/main/java/com/prgrms/mukvengers/domain/chat/model/MessageType.java
+++
@@ -1,7 +0,0 @@
-package com.prgrms.mukvengers.domain.chat.model;
-
-public enum MessageType {
-	CHAT,
-	JOIN,
-	LEAVE
-}
