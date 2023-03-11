33670fddbba2a778f02587575c901acab8cac05c
kwonjoosung <kwonjoosung@gmail.com>
2023-03-11 22:58:23 +0900

    refactor(Chat): ChatRepository JPQL 변경
    - N+1 문제를 방지하면서 필요한 User 정보도 가져올 수 있도록 JOIN과 DTO를 사용
    - 테스트 결과 Inner Join과 Limit 정상적으로 사용됨

--- src/main/java/com/prgrms/mukvengers/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/repository/ChatRepository.java
@@ -6,14 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse;
 import com.prgrms.mukvengers.domain.chat.model.Chat;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
 	@Query("""
-			SELECT c
-			FROM Chat c
+			SELECT 
+			new com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse
+			(u.id, u.nickname, u.profileImgUrl, c.createdAt, c.content) 
+			FROM Chat c 
+			JOIN User u ON u.id = c.userId 
 			WHERE c.crewId = :crewId
 		""")
-	Page<Chat> findByCrewId(@Param("crewId") Long crewId, Pageable pageable);
+	Page<ChatResponse> findByCrewId(@Param("crewId") Long crewId, Pageable pageable);
 }
