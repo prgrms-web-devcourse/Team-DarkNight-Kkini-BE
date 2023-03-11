80e42854f92ccfcc545c1096b780c74f086d58e8
kwonjoosung <kwonjoosung@gmail.com>
2023-03-11 23:22:00 +0900

    refactor(Chat): Map 데이터 파싱 메서드 추가
    - 추후에 Util로 빼는걸 고려해보겠습니다

--- src/main/java/com/prgrms/mukvengers/domain/chat/handler/WebSocketEventListener.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/handler/WebSocketEventListener.java
@@ -1,5 +1,8 @@
 package com.prgrms.mukvengers.domain.chat.handler;
 
+import java.util.Map;
+import java.util.Objects;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.event.EventListener;
@@ -12,6 +15,7 @@ import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import com.prgrms.mukvengers.domain.chat.dto.request.ChatRequest;
 import com.prgrms.mukvengers.domain.chat.dto.request.MessageType;
+import com.prgrms.mukvengers.domain.chat.exception.WebSocketException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -47,18 +51,42 @@ public class WebSocketEventListener {
 
 	@EventListener
 	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-		logger.info("web socket disconnected");
-		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
 
-		String username = (String)headerAccessor.getSessionAttributes().get("username");
-		Long userId = (Long)headerAccessor.getSessionAttributes().get("userId");
-		Long crewId = (Long)headerAccessor.getSessionAttributes().get("crewId");
+		String username = (String)getValue(accessor, "username");
+		Long userId = (Long)getValue(accessor, "userId");
+		Long crewId = (Long)getValue(accessor, "crewId");
 
-		if (username != null && userId != null && crewId != null) {
-			logger.info("User: {} {} Disconnected Crew : {}", userId, username, crewId);
-			ChatRequest chatRequest = new ChatRequest(MessageType.LEAVE, userId,
-				username + " 님이 떠났습니다.");
-			messagingTemplate.convertAndSend("/topic/public/" + crewId, chatRequest);
+		logger.info("User: {} {} Disconnected Crew : {}", userId, username, crewId);
+
+		ChatRequest chatRequest = new ChatRequest(
+			MessageType.LEAVE, userId, username + " 님이 떠났습니다.");
+
+		messagingTemplate.convertAndSend("/topic/public/" + crewId, chatRequest);
+	}
+
+	private Object getValue(StompHeaderAccessor accessor, String key) {
+		Map<String, Object> sessionAttributes = getSessionAttributes(accessor);
+		Object value = sessionAttributes.get(key);
+
+		if (Objects.isNull(value)) {
+			throw new WebSocketException(key + " 에 해당하는 값이 없습니다.");
 		}
+
+		return value;
+	}
+
+	private void setValue(StompHeaderAccessor accessor, String key, Object value) {
+		Map<String, Object> sessionAttributes = getSessionAttributes(accessor);
+		sessionAttributes.put(key, value);
+	}
+
+	private Map<String, Object> getSessionAttributes(StompHeaderAccessor accessor) {
+		Map<String, Object> sessionAttributes = accessor.getSessionAttributes();
+
+		if (Objects.isNull(sessionAttributes)) {
+			throw new WebSocketException("SessionAttributes가 null입니다.");
+		}
+		return sessionAttributes;
 	}
 }
