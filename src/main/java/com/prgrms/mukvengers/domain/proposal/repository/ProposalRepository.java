1c3b0b157638594ff5d9a8f2efd796ff27141fd0
DESKTOP-QJMFL6F\82104 <gw980215@naver.com>
2023-03-11 15:51:14 +0900

    feat(Proposal): 사용자 아이디와 모임 아이디로 신청서 상태 조회 기능 추가

--- src/main/java/com/prgrms/mukvengers/domain/proposal/repository/ProposalRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/proposal/repository/ProposalRepository.java
@@ -1,16 +1,26 @@
 package com.prgrms.mukvengers.domain.proposal.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.prgrms.mukvengers.domain.proposal.model.Proposal;
+import com.prgrms.mukvengers.domain.proposal.model.vo.ProposalStatus;
 
 public interface ProposalRepository extends JpaRepository<Proposal, Long> {
 
 	List<Proposal> findAllByLeaderIdOrderByCreatedAtDesc(@Param("userId") Long userId);
 
 	List<Proposal> findAllByUserIdOrderByCreatedAtDesc(@Param("userId") Long userId);
+
+	@Query("""
+		SELECT p.status
+		FROM Proposal p
+		WHERE p.user.id = :userId AND p.crewId = :crewId
+		""")
+	Optional<ProposalStatus> findByUserIdAndCrewId(@Param("userId") Long userId, @Param("crewId") Long crewId);
 
 }
