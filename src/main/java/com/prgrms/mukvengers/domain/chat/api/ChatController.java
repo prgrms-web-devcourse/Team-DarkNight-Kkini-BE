0c3c6c5f1df3e3d0bad979d005c13cce66dc14cd
kwonjoosung <kwonjoosung@gmail.com>
2023-03-11 23:01:28 +0900

    refactor(Chat): ChatController 변경
    - 헤더를 매개변수로 받을 수 있게 함
    - 요청후 값을 추가하여 반환할 수 있도록 함
    - 불필요한 메서드 제거(addUser)
    - path 변경

--- src/main/java/com/prgrms/mukvengers/domain/chat/api/ChatController.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/api/ChatController.java
@@ -1,27 +1,29 @@
 package com.prgrms.mukvengers.domain.chat.api;
 
+import java.util.Map;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.prgrms.mukvengers.domain.chat.dto.request.ChatRequest;
+import com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse;
 import com.prgrms.mukvengers.domain.chat.dto.response.ChatsInCrew;
-import com.prgrms.mukvengers.domain.chat.model.ChatMessage;
 import com.prgrms.mukvengers.domain.chat.service.ChatService;
 import com.prgrms.mukvengers.global.common.dto.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-//TODO 퇴장 처리, username 처리,
 @Slf4j
 @Controller
 @RequiredArgsConstructor
@@ -37,11 +39,8 @@ public class ChatController {
 		return "index";
 	}
 
-	/**
-	 * 웹소켓 테스트용 임시 Endpoint
-	 */
 	@ResponseBody
-	@GetMapping("/chat/{crewId}")
+	@GetMapping("/api/v1/crews/{crewId}/chats")
 	public ResponseEntity<ApiResponse<ChatsInCrew>> getChattingList(
 		@PathVariable(name = "crewId") Long crewId,
 		@PageableDefault(sort = "createdAt") Pageable pageable) {
@@ -53,20 +52,10 @@ public class ChatController {
 
 	@MessageMapping("/chat.sendMessage/{crewId}")
 	@SendTo("/topic/public/{crewId}")
-	public ChatMessage sendMessage(@DestinationVariable Long crewId,
-		@Payload ChatMessage chatMessage
+	public ChatResponse sendMessage(@DestinationVariable Long crewId,
+		@Header("simpSessionAttributes") Map<String, Object> simpSessionAttributes,
+		@Payload ChatRequest chatRequest
 	) {
-		chatService.save(chatMessage, crewId);
-
-		return chatMessage;
-	}
-
-	@MessageMapping("/chat.addUser/{crewId}")
-	@SendTo("/topic/public/{crewId}")
-	public ChatMessage addUser(@DestinationVariable String crewId, @Payload ChatMessage chatMessage,
-		SimpMessageHeaderAccessor headerAccessor) {
-		headerAccessor.getSessionAttributes().put("username", chatMessage.sender());
-
-		return chatMessage;
+		return chatService.save(chatRequest, crewId, simpSessionAttributes);
 	}
 }
