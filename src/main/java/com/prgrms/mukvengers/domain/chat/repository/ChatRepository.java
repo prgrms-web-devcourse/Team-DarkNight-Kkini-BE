75100aae4dc09a170c5c92b896d84be06944c666
김기서 <5rne@naver.com>
2023-03-13 04:42:05 +0900

    feat: ChatResponse에 id필드 추가

--- src/main/java/com/prgrms/mukvengers/domain/chat/repository/ChatRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/chat/repository/ChatRepository.java
@@ -1,5 +1,7 @@
 package com.prgrms.mukvengers.domain.chat.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,10 +17,20 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
 	@Query("""
 			SELECT 
 			new com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse
-			(u.id, u.nickname, u.profileImgUrl, c.type, c.createdAt, c.content) 
+			(c.id, u.id, u.nickname, u.profileImgUrl, c.type, c.createdAt, c.content) 
 			FROM Chat c 
 			JOIN User u ON u.id = c.userId 
 			WHERE c.crewId = :crewId
 		""")
 	Page<ChatResponse> findByCrewId(@Param("crewId") Long crewId, Pageable pageable);
+
+	@Query("""
+			SELECT 
+			new com.prgrms.mukvengers.domain.chat.dto.response.ChatResponse
+			(c.id, u.id, u.nickname, u.profileImgUrl, c.type, c.createdAt, c.content) 
+			FROM Chat c 
+			JOIN User u ON u.id = c.userId 
+			WHERE c.crewId = :crewId
+		""")
+	List<ChatResponse> findAllByCrewId(@Param("crewId") Long crewId);
 }
