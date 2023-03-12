969ec85493b8a16a0aa06a9add25fec9495c008f
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-12 15:07:09 +0900

    refactor(Crew): RequestDto Error 메시지 추가

--- src/main/java/com/prgrms/mukvengers/domain/crew/dto/request/CreateCrewRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/dto/request/CreateCrewRequest.java
@@ -12,10 +12,10 @@ import com.prgrms.mukvengers.domain.store.dto.request.CreateStoreRequest;
 public record CreateCrewRequest(
 
 	@NotNull CreateStoreRequest createStoreRequest,
-	@NotBlank String name,
-	@NotNull LocalDateTime promiseTime,
-	@Min(value = 2) @Max(value = 8) Integer capacity,
-	@NotBlank String content,
-	@NotBlank String category
+	@NotBlank(message = "이름을 입력해 주세요") String name,
+	@NotNull(message = "약속 시간을 입력해 주세요") LocalDateTime promiseTime,
+	@Min(value = 2, message = "정원은 {value}명 이상이어야 합니다.") @Max(value = 8, message = "정원은 {value}명 이하여야 합니다. ") Integer capacity,
+	@NotBlank(message = "내용을 입력해 주세요") String content,
+	@NotBlank(message = "카테고리를 입력해 주세요") String category
 ) {
 }
--- src/main/java/com/prgrms/mukvengers/domain/store/dto/request/CreateStoreRequest.java
+++ src/main/java/com/prgrms/mukvengers/domain/store/dto/request/CreateStoreRequest.java
@@ -4,14 +4,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public record CreateStoreRequest(
-	@NotNull Double longitude,
-	@NotNull Double latitude,
-	@NotBlank String placeId,
-	@NotBlank String placeName,
-	@NotBlank String categories,
-	@NotBlank String roadAddressName,
+	@NotNull(message = "올바르지 않는 경도입니다.") Double longitude,
+	@NotNull(message = "올바르지 안흔 위도입니다.") Double latitude,
+	@NotBlank(message = "올바르지 않는 api 아이디입니다.") String placeId,
+	@NotBlank(message = "올바르지 않는 가게 이름입니다.") String placeName,
+	@NotBlank(message = "올바르지 않는 카테고리입니다.") String categories,
+	@NotBlank(message = "올바르지 않는 도로명 주소입니다.") String roadAddressName,
 	String photoUrls,
-	@NotBlank String kakaoPlaceUrl,
+	@NotBlank(message = "올바르지 않는 이미지 주소입니다.") String kakaoPlaceUrl,
 	String phoneNumber
 ) {
 }
