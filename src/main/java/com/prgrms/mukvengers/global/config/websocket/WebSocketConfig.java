d855db7e60422b2f3ab485e97691323f7ad76aa7
kwonjoosung <kwonjoosung@gmail.com>
2023-03-12 15:00:45 +0900

    fix(Security): webSocket Cors Path 변경

--- src/main/java/com/prgrms/mukvengers/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/prgrms/mukvengers/global/config/websocket/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/ws/*").setAllowedOrigins("*").withSockJS();
 	}
 
 	@Override
