96433f2a1b89e0c64c5a054d23fccfebfb205888
kwonjoosung <kwonjoosung@gmail.com>
2023-03-28 16:03:15 +0900

    feat(Crew): 시간이 지난 모임 삭제 메서드 추가
    - 시간을 매개변수로 받아 해당 시간이 지난 경우 status를 "FINISH"로 변경
    - 테스트 코드로 검증 완료

--- src/main/java/com/prgrms/mukvengers/domain/crew/repository/CrewRepository.java
+++ src/main/java/com/prgrms/mukvengers/domain/crew/repository/CrewRepository.java
@@ -1,11 +1,13 @@
 package com.prgrms.mukvengers.domain.crew.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,7 +17,7 @@ public interface CrewRepository extends JpaRepository<Crew, Long> {
 
 	// N+1 발생
 	@Query(value = """
-		SELECT c 
+		SELECT c
 		FROM Crew c
 		JOIN FETCH c.store s
 		WHERE s.placeId = :placeId
@@ -27,4 +29,13 @@ public interface CrewRepository extends JpaRepository<Crew, Long> {
 		"SELECT * FROM crew c "
 			+ "WHERE ST_DISTANCE_SPHERE(:location, c.location) < :distance AND c.status = 'RECRUITING'")
 	List<Crew> findAllByLocation(@Param("location") Point location, @Param("distance") int distance);
+
+	@Modifying
+	@Query(value = """
+		UPDATE Crew c
+		SET c.status = 'FINISH'
+		WHERE c.status != 'FINISH' AND c.promiseTime < :time
+		""")
+	int updateStatusAll(@Param("time")LocalDateTime now);
+
 }
