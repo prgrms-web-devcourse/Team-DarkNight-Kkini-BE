f445622d0d59886d11c8b91d96af0bcce1d0788e
Geonwoo Lee <82176176+geonwoo0215@users.noreply.github.com>
2023-03-29 15:40:10 +0900

    refactor(CrewMember): URI 매핑 수정

--- src/main/java/com/prgrms/mukvengers/domain/crewmember/api/CrewMemberController.java
+++ src/main/java/com/prgrms/mukvengers/domain/crewmember/api/CrewMemberController.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.mukvengers.domain.crewmember.dto.request.UpdateCrewMemberRequest;
@@ -20,8 +19,7 @@ public class CrewMemberController {
 
 	private final CrewMemberService crewMemberService;
 
-	@PatchMapping
-	@RequestMapping("/api/v1/crews/{crewId}/crewMembers")
+	@PatchMapping(value = "/api/v1/crews/{crewId}/crewMembers")
 	public ResponseEntity<Void> block(
 		@RequestBody UpdateCrewMemberRequest updateCrewMemberRequest,
 		@PathVariable Long crewId,
