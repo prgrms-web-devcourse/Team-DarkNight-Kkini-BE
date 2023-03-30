048a01b9ec5b9c20d1f14e374257b3f015664e63
kwonjoosung <kwonjoosung@gmail.com>
2023-03-30 22:50:35 +0900

    feat(Config): Scheduling 비동기로 설정

--- src/main/java/com/prgrms/mukvengers/global/scheduler/Scheduler.java
+++ src/main/java/com/prgrms/mukvengers/global/scheduler/Scheduler.java
@@ -2,6 +2,8 @@ package com.prgrms.mukvengers.global.scheduler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,6 +15,7 @@ import com.prgrms.mukvengers.domain.proposal.repository.ProposalRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
+@EnableAsync
 @EnableScheduling
 @RequiredArgsConstructor
 public class Scheduler {
@@ -20,12 +23,14 @@ public class Scheduler {
 	private final CrewRepository crewRepository;
 	private final ProposalRepository proposalRepository;
 
+	@Async
 	@Scheduled(cron = "0 0 * * * *", zone = "Asia/Seoul")
 	@Transactional
 	public void changStatusOvertimeCrew() {
 		crewRepository.updateAllStatusToFinish(LocalDateTime.now());
 	}
 
+	@Async
 	@Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul")
 	@Transactional
 	public void deleteProposalOverTimeCrew() {
