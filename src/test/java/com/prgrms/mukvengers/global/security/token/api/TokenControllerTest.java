b992fa995d88422493e57c8cc2eebb1df30592e9
kwonjoosung <kwonjoosung@gmail.com>
2023-05-09 00:46:29 +0900

    fix: TokenControllerTest 서비스 Mocking 하기
    - 실제 저장되는 과정이 레디스에 의존적이어서 mocking으로 처리했습니다
    - 추후에 임베디드 레디스를 적용해보면 좋을것 같네요

--- src/test/java/com/prgrms/mukvengers/global/security/token/api/TokenControllerTest.java
+++ src/test/java/com/prgrms/mukvengers/global/security/token/api/TokenControllerTest.java
@@ -1,5 +1,6 @@
 package com.prgrms.mukvengers.global.security.token.api;
 
+import static org.mockito.BDDMockito.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -9,35 +10,29 @@ import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 
 import com.prgrms.mukvengers.base.ControllerTest;
-import com.prgrms.mukvengers.global.security.oauth.dto.AuthUserInfo;
-import com.prgrms.mukvengers.global.security.token.dto.Tokens;
+import com.prgrms.mukvengers.global.security.token.exception.RefreshTokenNotFoundException;
 import com.prgrms.mukvengers.global.security.token.service.TokenService;
 
 class TokenControllerTest extends ControllerTest {
 
 	private final String invalidRefreshToken = "invalidRefreshToken";
-
-	@Autowired
+	private final String validRefreshToken = "validRefreshToken";
+	@MockBean
 	private TokenService tokenservice;
-
-	private String validRefreshToken;
 
 	@BeforeEach
 	void setUp() {
-
-		AuthUserInfo userInfo = new AuthUserInfo(savedUser1Id, "Test", "USER");
-		Tokens tokens = tokenservice.createTokens(userInfo);
-		validRefreshToken = tokens.refreshToken();
+		given(tokenservice.getAccessTokensByRefreshToken(any(String.class))).willReturn(validRefreshToken);
 	}
 
 	@Test
 	@DisplayName("[성공] refresh token으로 access token 재발급")
 	void refreshAccessTokenTest_success() throws Exception {
-
+		given(tokenservice.getAccessTokensByRefreshToken(any(String.class))).willReturn(validRefreshToken);
 		mockMvc.perform(post("/api/v1/tokens")
 				.cookie(new Cookie("refreshToken", validRefreshToken)))
 			.andExpect(status().isOk())
@@ -49,6 +44,8 @@ class TokenControllerTest extends ControllerTest {
 	@Test
 	@DisplayName("[실패] 재발급 실패 : refresh token이 올바르지 않은 경우 - 401")
 	void refreshAccessTokenTest_fai_invalid() throws Exception {
+		willThrow(RefreshTokenNotFoundException.class).given(
+			tokenservice.getAccessTokensByRefreshToken(any(String.class)));
 		mockMvc.perform(post("/api/v1/tokens")
 				.cookie(new Cookie("refreshToken", invalidRefreshToken)))
 			.andExpect(status().is4xxClientError())
@@ -67,6 +64,7 @@ class TokenControllerTest extends ControllerTest {
 	@Test
 	@DisplayName("[성공] 로그 아웃 - refresh token 삭제")
 	void expireRefreshTokenTes_success() throws Exception { // 존재하지 않더라도 실패 X
+		willDoNothing().given(tokenservice).deleteRefreshToken(any(String.class));
 		mockMvc.perform(delete("/api/v1/tokens")
 				.cookie(new Cookie("refreshToken", validRefreshToken)))
 			.andExpect(status().isNoContent())
