39b1e836d17698538c27a7960c5747a9b5d5d525
김기서 <5rne@naver.com>
2023-05-10 15:56:21 +0900

    test: notification 도메인, repository 계층 테스트 추가

--- src/test/java/com/prgrms/mukvengers/domain/notification/model/NotificationTest.java
+++ src/test/java/com/prgrms/mukvengers/domain/notification/model/NotificationTest.java
@@ -1,6 +1,7 @@
 package com.prgrms.mukvengers.domain.notification.model;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -10,83 +11,93 @@ import org.junit.jupiter.params.provider.NullSource;
 import com.prgrms.mukvengers.domain.notification.exception.InvalidNotificationContentException;
 import com.prgrms.mukvengers.domain.notification.model.vo.NotificationContent;
 import com.prgrms.mukvengers.domain.notification.model.vo.NotificationType;
-import com.prgrms.mukvengers.domain.user.model.User;
-import com.prgrms.mukvengers.utils.UserObjectProvider;
 
 class NotificationTest {
 
 	@ParameterizedTest
 	@NullSource
 	@EmptySource
-	@DisplayName("알림 내용이 공백일 경우 커스텀 예외가 발생한다.")
+	@DisplayName("[실패] 알림 내용이 공백일 경우 커스텀 예외가 발생한다.")
 	public void createNotificationContent_fail(String content) {
 		//given
 
 		//when, then
-		Assertions.assertThrows(InvalidNotificationContentException.class, () -> new NotificationContent(content));
+		assertThrows(InvalidNotificationContentException.class, () -> new NotificationContent(content));
 	}
 
 	@Test
-	@DisplayName("알림 내용이 50글자 이상일 경우 커스텀 예외가 발생한다.")
+	@DisplayName("[실패] 알림 내용이 50글자 이상일 경우 커스텀 예외가 발생한다.")
 	public void createNotificationContent_fail2() {
 		//given
 
 		//when, then
-		Assertions.assertThrows(InvalidNotificationContentException.class,
+		assertThrows(InvalidNotificationContentException.class,
 			() -> new NotificationContent("hh".repeat(30)));
 	}
 
 	@Test
-	@DisplayName("알림 내용이 성공적으로 생성된다.")
+	@DisplayName("[성공] 알림 내용이 성공적으로 생성된다.")
 	public void createNotificationContent_success() {
 		//given
 
 		//when, then
-		Assertions.assertDoesNotThrow(() -> new NotificationContent("밥먹자"));
+		assertDoesNotThrow(() -> new NotificationContent("밥먹자"));
 	}
 
 	@Test
-	@DisplayName("Type 정보를 가진 알림이 성공적으로 생성된다.")
+	@DisplayName("[성공] Type 정보를 가진 알림이 성공적으로 생성된다.")
 	public void createNotification_success() {
 		//given
-		User user = UserObjectProvider.createUser();
 		String content = "신청서 도착했어용";
 		//when, then
 
-		Assertions.assertDoesNotThrow(() -> new Notification(content, false, user, NotificationType.CONNECT));
-		Assertions.assertDoesNotThrow(() -> new Notification(content, false, user, NotificationType.APPROVE));
-		Assertions.assertDoesNotThrow(() -> new Notification(content, false, user, NotificationType.REJECT));
+		assertDoesNotThrow(() -> new Notification(content, false, 1L, NotificationType.CONNECT));
+		assertDoesNotThrow(() -> new Notification(content, false, 1L, NotificationType.APPROVE));
+		assertDoesNotThrow(() -> new Notification(content, false, 1L, NotificationType.REJECT));
 	}
 
 	@ParameterizedTest
 	@NullSource
-	@DisplayName("Type 정보가 없는 경우 예외가 발생한다.")
+	@DisplayName("[실패] Type 정보가 없는 경우 예외가 발생한다.")
 	public void invalidType_fail(NotificationType type) {
 		//given
-		User user = UserObjectProvider.createUser();
 		String content = "신청서 도착했어용";
 		//when, then
 
-		Assertions.assertThrows(IllegalArgumentException.class,
-			() -> new Notification(content, false, user, type));
+		assertThrows(IllegalArgumentException.class,
+			() -> new Notification(content, false, 1L, type));
 	}
 
 	@ParameterizedTest
 	@NullSource
-	@DisplayName("receiver 정보가 없는 경우 예외가 발생한다.")
-	public void invalidReceiver_fail(User receiver) {
+	@DisplayName("[실패] receiver 정보가 없는 경우 예외가 발생한다.")
+	public void invalidReceiver_fail(Long receiverId) {
 		//given
 		String content = "신청서 도착했어용";
 
 		//when, then
-		Assertions.assertThrows(IllegalArgumentException.class,
-			() -> new Notification(content, false, receiver, NotificationType.CONNECT));
+		assertThrows(IllegalArgumentException.class,
+			() -> new Notification(content, false, receiverId, NotificationType.CONNECT));
 
-		Assertions.assertThrows(IllegalArgumentException.class,
-			() -> new Notification(content, false, receiver, NotificationType.APPROVE));
+		assertThrows(IllegalArgumentException.class,
+			() -> new Notification(content, false, receiverId, NotificationType.APPROVE));
 
-		Assertions.assertThrows(IllegalArgumentException.class,
-			() -> new Notification(content, false, receiver, NotificationType.REJECT));
+		assertThrows(IllegalArgumentException.class,
+			() -> new Notification(content, false, receiverId, NotificationType.REJECT));
 	}
 
+	@Test
+	@DisplayName("[성공] 알림 메세지를 읽음 처리한다.")
+	public void read() {
+		//given
+		String content = "밥먹자";
+		Notification notification = new Notification(content, false, 1L, NotificationType.CONNECT);
+		assertEquals(false, notification.getIsRead());
+
+		//when
+		notification.read();
+
+		//then
+		assertEquals(true, notification.getIsRead());
+	}
 }
