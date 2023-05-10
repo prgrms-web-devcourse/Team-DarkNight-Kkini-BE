47a92bab4be310006868be0c9e490bc5064cbf39
김기서 <5rne@naver.com>
2023-05-10 21:15:52 +0900

    chore: flyway 수정 (isRead 타입 변경)

--- src/main/resources/db/migration/V4.2__alter_notification_table.sql
+++ src/main/resources/db/migration/V4.2__alter_notification_table.sql
@@ -1,10 +1,9 @@
 # 컬럼명 수정: isRead를 is_read로 변경
 ALTER TABLE notification
-    CHANGE isRead is_read boolean;
+    CHANGE isRead is_read Boolean;
 
 ALTER TABLE notification
     CHANGE receiver receiver_id bigint;
-
 
 # receiver_id 인덱스 추가
 create index idx_receiver_id on notification (receiver_id);
