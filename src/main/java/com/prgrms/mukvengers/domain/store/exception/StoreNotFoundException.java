5109f2babec6f399ba5806a8f4da3a6dadb8207b
김기서 <5rne@naver.com>
2023-05-17 20:18:35 +0900

    chore: typo 수정 및 에러코드 정정

--- src/main/java/com/prgrms/mukvengers/domain/store/exception/StoreNotFoundException.java
+++ src/main/java/com/prgrms/mukvengers/domain/store/exception/StoreNotFoundException.java
@@ -5,7 +5,7 @@ import com.prgrms.mukvengers.global.exception.ServiceException;
 
 public class StoreNotFoundException extends ServiceException {
 
-	private static final ErrorCode ERROR_CODE = ErrorCode.CREW_NOT_FOUND;
+	private static final ErrorCode ERROR_CODE = ErrorCode.STORE_NOT_FOUND;
 	private static final String MESSAGE_KEY = "exception.store.notfound";
 
 	public StoreNotFoundException(Long storeId) {
