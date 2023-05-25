edb0f75b37b861d651111b103b5d796074f91fec
kwonjoosung <kwonjoosung@gmail.com>
2023-05-25 22:06:24 +0900

    feat: CacheConfig & redisCacheManager 추가
    - EnableCaching
    - RedisCacheManager Bean 등록

---
+++ src/main/java/com/prgrms/mukvengers/global/config/cache/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.prgrms.mukvengers.global.config.cache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+}
